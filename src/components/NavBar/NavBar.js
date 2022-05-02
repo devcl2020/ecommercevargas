5223db331d12ab213a7b8a8e40fdf9b295d5c8dc
Alejandro Vargas <alejandro.vargas@helice.cl>
2022-05-02 12:57:01 -0400

    tarea NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+
+
+
+function NavBar() {
+    return (
+        <div className="App">
+           <h1>NAVBAR</h1>
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ src/pages/productos.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Inicio = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '190vh'
+      }}
+    >
+      <h1>ABOUUUUUT</h1>
+    </div>
+  );
+};
+
+export default Inicio;

722bafafb9ca3d3ff301547d33855f889cdf15ac
Alejandro Vargas <alejandro.vargas@helice.cl>
2022-05-27 12:17:23 -0400

    update del Proyecto

---
+++ src/components/RopaCard.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+const RopaCard = ({ data }) => {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component='img'
+                height='440'
+                image={data.img}
+                alt='green iguana'
+            />
+            <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                    Name: {data.descripcion}
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                    Status: {data.precio}
+                </Typography>
+
+            </CardContent>
+        </Card>
+    );
+};
+
+export default RopaCard;
